MotorolaMobilityLLC/system-core

JellyBean upmerge for MME recording framework                   Merge for env callback support
Change-Id: I4b8d8c59c08a8111db129c73ad428656478db10d
Change-Id: Ib68d13cf7fe94a6fb7f50d5588260a56ff8b816f
Reviewed-on: http://gerrit.pcs.mot.com/457077
Tested-by: Jira Key <JIRAKEY@motorola.com>
Reviewed-by: Xin Yang <e8110c@motorola.com>
(cherry picked from commit 60c587c23252e7148a54de84d9b2804cd7783b27)

include/system/camera.h
@@ -90,6 +90,13 @@ enum {
     CAMERA_MSG_FOCUS_MOVE = 0x0800,       // notifyCallback
     CAMERA_MSG_STATS_DATA = 0x1000,
     CAMERA_MSG_META_DATA = 0x2000,
+
+    // BEGIN Motorola, w4779c, 2012-02-17, IKHSS6-8697
+    // Define Motorola-specific camera interface. It is Motorola-specific msg
+    // to pass quality related information from HAL back to Camera app.
+    CAMERA_MSG_ENV_DETECT = 0x8000,
+    // END IKHSS6-8697
+
     CAMERA_MSG_ALL_MSGS = 0xFFFF
 };
 

altusnets/system-core-pie

libsystem/include/system/camera.h
@@ -91,6 +91,13 @@ enum {
     CAMERA_MSG_VENDOR_START = 0x1000,
     CAMERA_MSG_STATS_DATA = CAMERA_MSG_VENDOR_START,
     CAMERA_MSG_META_DATA = 0x2000,
+
+    // BEGIN Motorola, w4779c, 2012-02-17, IKHSS6-8697
+    // Define Motorola-specific camera interface. It is Motorola-specific msg
+    // to pass quality related information from HAL back to Camera app.
+    CAMERA_MSG_ENV_DETECT = 0x4000,
+    // END IKHSS6-8697
+
     CAMERA_MSG_VENDOR_END = 0x8000,
     CAMERA_MSG_ALL_MSGS = 0xFFFF
 };