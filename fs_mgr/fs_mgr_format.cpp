MotorolaMobilityLLC/system-core

change f2fs format tool name to make_f2fs
From O MR1, Google AOSP include 1.8.0 f2fs-tools in build. It added
make_f2fs instead of mkfs.f2fs, mkfs.f2fs would be use for recovery
only. Better to keep same name with AOSP.

Change-Id: I7a4388b5dc4101bc0c7121311dfc32738f4130f0
Reviewed-on: https://gerrit.mot.com/1140878
SLTApproved: Slta Waiver
SME-Granted: SME Approvals Granted
Tested-by: Jira Key
Reviewed-by: Shi-Yong Li <a22381@motorola.com>
Submit-Approved: Shi-Yong Li <a22381@motorola.com>
Reviewed-by: Andrew Wheeler <awheeler@motorola.com>

fs_mgr/fs_mgr_format.cpp
@@ -100,7 +100,7 @@ static int format_ext4(char *fs_blkdev, char *fs_mnt_point, bool crypt_footer)
     return rc;
 }
 
-#define MKFS_F2FS_PATH "/vendor/bin/mkfs.f2fs"
+#define MKFS_F2FS_PATH "/vendor/bin/make_f2fs"
 #define MKFS_SECURITY_CONTEXT "u:r:mkfs:s0"
 static int format_f2fs(char *fs_blkdev, bool crypt_footer)
 {

fs_mgr/recover_userdata.c
@@ -139,14 +139,14 @@ int format_fs(char *fs_type, char *fs_real_blkdev, char *fs_mnt_point, long int 
         char * args[5];
         char footer_size[10];
         snprintf(footer_size, sizeof(footer_size), "%d", CRYPT_FOOTER_OFFSET);
-        args[0] = (char *)"/system/bin/mkfs.f2fs_arm";
+        args[0] = (char *)"/vendor/bin/make_f2fs";
         args[1] = (char *)"-r";
         args[2] = footer_size;
         args[3] = fs_real_blkdev;
         args[4] = (char *)0;
 	if (!fork()) {
 		/* This doesn't return */
-		execv("/system/bin/mkfs.f2fs_arm", args);
+		execv("/vendor/bin/make_f2fs", args);
 		return -1;
 	}
 	wait(&status);

altusnets/system-core-pie

fs_mgr/fs_mgr_format.cpp
@@ -112,7 +112,7 @@ static int format_ext4(char *fs_blkdev, char *fs_mnt_point, bool crypt_footer)
     return rc;
 }
 
-#define MKFS_F2FS_PATH "/vendor/bin/mkfs.f2fs"
+#define MKFS_F2FS_PATH "/system/bin/make_f2fs"
 #define MKFS_SECURITY_CONTEXT "u:r:mkfs:s0"
 static int format_f2fs(char *fs_blkdev, uint64_t dev_sz, bool crypt_footer)
 {

fs_mgr/recover_userdata.c
@@ -139,14 +139,14 @@ int format_fs(char *fs_type, char *fs_real_blkdev, char *fs_mnt_point, long int 
         char * args[5];
         char footer_size[10];
         snprintf(footer_size, sizeof(footer_size), "%d", CRYPT_FOOTER_OFFSET);
-        args[0] = (char *)"/system/bin/mkfs.f2fs_arm";
+        args[0] = (char *)"/system/bin/make_f2fs";
         args[1] = (char *)"-r";
         args[2] = footer_size;
         args[3] = fs_real_blkdev;
         args[4] = (char *)0;
 	if (!fork()) {
 		/* This doesn't return */
-		execv("/system/bin/mkfs.f2fs_arm", args);
+		execv("/system/bin/make_f2fs", args);
 		return -1;
 	}
 	wait(&status);